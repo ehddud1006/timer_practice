b577f90e0981759cd464f17def7b40a8052461ed
ehddud1006 <62373865+ehddud1006@users.noreply.github.com>
2022-04-30 17:30:24 +0900

    dev

--- src/App.js
+++ src/App.js
@@ -7,9 +7,8 @@ export default function App() {
   // const THREE_DAYS_IN_MS = 3 * 24 * 60 * 60 * 1000;
   const THREE_DAYS_IN_MS = 1 * 1 * 1 * 10 * 1000;
   const NOW_IN_MS = new Date().getTime();
-
-  const dateTimeAfterThreeDays = NOW_IN_MS + THREE_DAYS_IN_MS;
-
+  const dateTimeAfterThreeDays = 60;
+  console.log("dateTimeAfterThreeDays");
   return (
     <div>
       <h1>Countdown Timer</h1>
